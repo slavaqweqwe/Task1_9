d6bb984fa0c430287698a6e63e4e791fc4c41387
slavaqweqwe <74615552+slavaqweqwe@users.noreply.github.com>
2021-04-10 19:18:48 +0300

    1

---
+++ my_task/task.h
@@ -0,0 +1,34 @@
+﻿// my_sin.h : включаемый файл для стандартных системных включаемых файлов
+// или включаемые файлы для конкретного проекта.
+
+#pragma once
+
+#include "vector"
+namespace testspace {
+
+    class ParentClass
+    {
+    public:
+        virtual std::vector<uint8_t> Encode(std::vector<uint8_t> const& data) = 0;
+    };
+
+
+    class Hamming74 :public ParentClass
+    {
+    private:
+        uint8_t sum4BitsMod2(uint8_t x);
+    public:
+        virtual std::vector<uint8_t> Encode(std::vector<uint8_t> const& data);
+    };
+
+
+    class DiffCode :public ParentClass
+    {
+
+    public:
+        virtual std::vector<uint8_t> Encode(std::vector<uint8_t> const& data);
+    };
+
+
+
+}
---
+++ my_task/tests/tests.cpp
@@ -0,0 +1,28 @@
+﻿// my_sin.cpp: определяет точку входа для приложения.
+//
+
+#include <gtest/gtest.h>
+#include "task.h"
+using namespace testspace;
+TEST(task_lib_test, Hamming)
+{
+    Hamming74 D;
+    std::vector<uint8_t> in = { 1, 2, 3, 4};
+    std::vector<uint8_t> teor ={ 81, 128, 28, 48, 2, 208, 0};
+    std::vector<uint8_t> En = D.Encode(in);
+    ASSERT_EQ(En, teor);
+}
+TEST(task_lib_test, Diff)
+{
+    DiffCode D;
+    std::vector<uint8_t> in = { 1, 2, 3, 4 };
+    std::vector<uint8_t> teor = { 255, 1, 1, 252};
+    std::vector<uint8_t> En = D.Encode(in);
+    ASSERT_EQ(En, teor);
+}
+
+int main(int argc, char* argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
