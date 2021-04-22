5f951048281217b23c3754736f5ecb14b770ba9d
slavaqweqwe <74615552+slavaqweqwe@users.noreply.github.com>
2021-04-22 10:25:28 +0300

    4

--- my_task/task.h
+++ my_task/task.h
@@ -1,32 +1,30 @@
-﻿// my_sin.h : включаемый файл для стандартных системных включаемых файлов
-// или включаемые файлы для конкретного проекта.
-
+﻿
 #pragma once
 
 #include "vector"
 namespace testspace {
 
-    class ParentClass
+    class ICoder
     {
     public:
         virtual std::vector<uint8_t> Encode(std::vector<uint8_t> const& data) = 0;
     };
 
 
-    class Hamming74 :public ParentClass
+    class Hamming74 :public ICoder
     {
     private:
         uint8_t sum4BitsMod2(uint8_t x);
     public:
-        virtual std::vector<uint8_t> Encode(std::vector<uint8_t> const& data);
+        std::vector<uint8_t> Encode(std::vector<uint8_t> const& data) override;
     };
 
 
-    class DiffCode :public ParentClass
+    class DiffCode :public ICoder
     {
 
     public:
-        virtual std::vector<uint8_t> Encode(std::vector<uint8_t> const& data);
+        virtual std::vector<uint8_t> Encode(std::vector<uint8_t> const& data) override;
     };
 
 
--- my_task/tests/tests.cpp
+++ my_task/tests/tests.cpp
@@ -1,22 +1,29 @@
-﻿// my_sin.cpp: определяет точку входа для приложения.
-//
-
+﻿
 #include <gtest/gtest.h>
 #include "task.h"
 using namespace testspace;
 TEST(task_lib_test, Hamming)
 {
     Hamming74 D;
-    std::vector<uint8_t> in = { 1, 2, 3, 4};
-    std::vector<uint8_t> teor ={ 81, 128, 28, 48, 2, 208, 0};
+    std::vector<uint8_t> in = { 1, 2, 3, 4 };
+    std::vector<uint8_t> teor = { 81, 128, 28, 48, 2, 208, 0 };
     std::vector<uint8_t> En = D.Encode(in);
+    ASSERT_EQ(En, teor);
+}
+TEST(task_lib_test, Hamming2)
+{
+    Hamming74 Ham;
+    ICoder* pICoder = &Ham;
+    std::vector<uint8_t> in = { 1, 2, 3, 4 };
+    std::vector<uint8_t> teor = { 81, 128, 28, 48, 2, 208, 0 };
+    std::vector<uint8_t> En = pICoder->Encode(in);
     ASSERT_EQ(En, teor);
 }
 TEST(task_lib_test, Diff)
 {
     DiffCode D;
     std::vector<uint8_t> in = { 1, 2, 3, 4 };
-    std::vector<uint8_t> teor = { 255, 1, 1, 252};
+    std::vector<uint8_t> teor = { 255, 1, 1, 252 };
     std::vector<uint8_t> En = D.Encode(in);
     ASSERT_EQ(En, teor);
 }
